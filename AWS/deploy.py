e8b7f608e042f8cd68a33ba463e6176ad0623d9c
nexoVI <38692216+nexoVI@users.noreply.github.com>
2021-10-19 00:55:40 +0200

    Add files via upload

---
+++ AWS/deploy.py
@@ -0,0 +1,12 @@
+import boto3
+
+ec2 = boto3.resource('ec2')
+
+# create a new EC2 instance
+instances = ec2.create_instances(
+     ImageId='ami-00399ec92321828f5',
+     MinCount=1,
+     MaxCount=2,
+     InstanceType='t2.micro',
+     KeyName='ec2-keypair2'
+ )
